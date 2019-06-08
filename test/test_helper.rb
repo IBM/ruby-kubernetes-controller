2430dec828d5cbc476854ab26b0c8612447fcdcd
Shivansh Vij <shivanshvij@outlook.com>
2019-06-07 22:10:58 -0400

    Adding continuous integration logging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "rubykubernetescontroller"
 
+require "minitest"
 require "minitest/autorun"
+require "minitest/ci"
+
+if ENV["CIRCLECI"]
+  Minitest::Ci.report_dir = "test/reports"
+end
